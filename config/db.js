bd941fa7636289ba66dfb3c1b63b0a35e16df295
Aakash-Bc <134195553+Aakash-Bc@users.noreply.github.com>
2026-04-25 18:56:47 +0545

    dbjs work

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL);
-        console.log("MongoDB Connected ✅");
-    } catch (error) {
-        console.log("Database Connection Failed ❌");
-    }
+  try {
+    console.log("DB URL exists:", !!process.env.DB_URL);
+
+    await mongoose.connect(process.env.DB_URL, {
+      serverSelectionTimeoutMS: 10000,
+    });
+
+    console.log("MongoDB Connected ✅");
+  } catch (error) {
+    console.log("Database Connection Failed ❌");
+    console.log("Error:", error.message);
+  }
 };
